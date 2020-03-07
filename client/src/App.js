b61b540d545a2ee151f5bfb221e9fd805776ab6a
Marcus Price <marcusprice88@gmail.com>
2020-03-07 10:58:11 -0800

    basic geolocation working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Map from './components/Map';
 
 const App = () => {
+  const [userLocation, setUserLocation] = useState({ enabled: false, lat: 47.7511, lng: -120.7401  }); //defaults to washington coordinates
+
+  //gets user's position for the map & hike radius
+  navigator.geolocation.watchPosition((position) => {
+    setUserLocation({
+      enabled: true,
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    });
+  })
+
   return (
-    <Map />
+    <Map userLocation={userLocation} />
   );
 }
 
--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Map = () => {
+const Map = (props) => {
+  const [map, setMap] = useState(null); //this never really changes after it's set, but state is the best solution to maintain the map on rerender
 
   //loads map after initial render (only runs once)
   useEffect(() => {
     //first add the initMap method to the window object (called by google maps uri callback)
     window.initMap = () => {
-      //initMap calls the map logic after the google maps resources have been loaded
-      mywtaMap(window.google);
+      //initMap calls createGoogleMap after the google maps resources have been loaded
+      createGoogleMap(window.google);
       //init map no longer needed
       delete window.initMap;
     }
@@ -18,22 +19,31 @@ const Map = () => {
     googleScript.defer = true;
     googleScript.src = 'https://maps.googleapis.com/maps/api/js?key=' + process.env.REACT_APP_GOOGLE_MAPS_API_KEY + '&callback=initMap';  //calls initMap onload
     document.getElementsByTagName('body')[0].appendChild(googleScript);
+
+    //creates a google map and ads it to state
+    const createGoogleMap = (google) => {
+      //intialize with WA coordinates
+      const wa = { lat: 47.7511, lng: -120.7401 };
+      //the map
+      const googleMap = new google.maps.Map(
+        document.getElementById('map'), {
+          disableDefaultUI: true,
+          zoomControl: true,
+          zoom: 14,
+          center: wa,
+        }
+      );
+
+      setMap(googleMap);
+    }
   }, []);
 
-  //mywta map logic
-  const mywtaMap = (google) => {
-    //washington coordinates
-    var wa = { lat: 47.7511, lng: -120.7401 };
-    //the map
-    var map = new google.maps.Map(
-      document.getElementById('map'), {
-        disableDefaultUI: true,
-        zoomControl: true,
-        zoom: 6,
-        center: wa,
-      }
-    );
-  }
+  //update map when user location changes
+  useEffect(() => {
+    if(map && props.userLocation.enabled) {
+      map.setCenter({ lat: props.userLocation.lat, lng: props.userLocation.lng })
+    }
+  }, [props.userLocation, map]);
 
   return(
     <div id="map" />
