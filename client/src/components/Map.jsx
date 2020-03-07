b29a88e7bcc675b9784713872cf86c4b1204a24e
Marcus Price <marcusprice88@gmail.com>
2020-03-07 09:17:52 -0800

    initial map has been integrated

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 
 const Map = () => {
 
-  //loads map aftrer initial render (only runs once)
+  //loads map after initial render (only runs once)
   useEffect(() => {
-    //first add the initMap method to the window object
+    //first add the initMap method to the window object (called by google maps uri callback)
     window.initMap = () => {
       //initMap calls the map logic after the google maps resources have been loaded
       mywtaMap(window.google);
@@ -16,24 +16,23 @@ const Map = () => {
     let googleScript = document.createElement('script');
     googleScript.async = true;
     googleScript.defer = true;
-    googleScript.src = 'https://maps.googleapis.com/maps/api/js?key=' + process.env.REACT_APP_GOOGLE_MAPS_API_KEY + '&callback=initMap';
+    googleScript.src = 'https://maps.googleapis.com/maps/api/js?key=' + process.env.REACT_APP_GOOGLE_MAPS_API_KEY + '&callback=initMap';  //calls initMap onload
     document.getElementsByTagName('body')[0].appendChild(googleScript);
   }, []);
 
   //mywta map logic
   const mywtaMap = (google) => {
     //washington coordinates
-    var wa = {lat: 47.7511, lng: -120.7401};
-    //the map object
+    var wa = { lat: 47.7511, lng: -120.7401 };
+    //the map
     var map = new google.maps.Map(
       document.getElementById('map'), {
+        disableDefaultUI: true,
+        zoomControl: true,
         zoom: 6,
-        center: wa
+        center: wa,
       }
     );
-
-    //a marker centered in washinton
-    new google.maps.Marker({position: wa, map: map});
   }
 
   return(
