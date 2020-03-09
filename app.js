6c17537e52570a40d150dc47099dc7d33d8e2cf4
Marcus Price <marcusprice88@gmail.com>
2020-03-08 17:24:17 -0700

    made production build

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000
 
+app.use(express.static('./client/build'));
+
+app.get('/', (req, res) => {
+  res.send('./client/build/index.html');
+})
+
 app.listen(port, () => {
   console.log('listening on port ' + port);
 })
---
+++ client/build/precache-manifest.a72ae7395b1b10509535ecce41a79432.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31cd6507fa05723c6c5177ca5a24fbfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d4875e831c92a2fe66",
+    "url": "/static/css/main.cd5a6ca1.chunk.css"
+  },
+  {
+    "revision": "d4601d7203866728e0fd",
+    "url": "/static/js/2.1b100024.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1b100024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d4875e831c92a2fe66",
+    "url": "/static/js/main.fe30c43a.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "096e9c83356017cb5305ba598011cb22",
+    "url": "/static/media/about.096e9c83.svg"
+  },
+  {
+    "revision": "9ae344612e5a8e51dc5b43521dfa06f7",
+    "url": "/static/media/hike-info.9ae34461.svg"
+  },
+  {
+    "revision": "4eb24737cf1e5197bd7107cc6573b78a",
+    "url": "/static/media/info.4eb24737.svg"
+  },
+  {
+    "revision": "84b8f6ae0e59db7d7868dbbd6c866384",
+    "url": "/static/media/location.84b8f6ae.svg"
+  },
+  {
+    "revision": "d4531081215c6523986ebc509c0a87ec",
+    "url": "/static/media/map.d4531081.svg"
+  },
+  {
+    "revision": "698ee69ad9c9b7df99f83e375dad4925",
+    "url": "/static/media/search.698ee69a.svg"
+  }
+]);
