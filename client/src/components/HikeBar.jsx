fd142c34071a81a3e92487d7d0c9d4397ae51cd0
Marcus Price <marcusprice88@gmail.com>
2020-03-08 17:02:43 -0700

    added font and icon to hike bar

--- client/src/components/HikeBar.jsx
+++ client/src/components/HikeBar.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import info from '../assets/icons/info.svg';
 
 const HikeBar = () => {
   return(
-    <div className="hike-bar">mywta</div>
+    <div className="hike-bar">
+      <span className="hike-bar-title">mywta</span>
+      <img className="info-icon" src={info} alt="info icon"/>
+    </div>
   )
 }
 
