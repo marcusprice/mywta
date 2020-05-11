c260e19aa7e1c99add086f7c7cc53a9b1328ae30
Marcus Price <marcusprice88@gmail.com>
2020-05-11 12:57:40 -0700

    moved hike parameters to search component

--- client/src/App.js
+++ client/src/App.js
@@ -10,43 +10,6 @@ const App = () => {
   const [contentWindowExpanded, setContentWindowExpanded] = useState(false);
   //const [userLocation, setUserLocation] = useState({ enabled: false, lat: 47.7511, lng: -120.7401, accuracy: 0 }); //defaults to washington coordinates
   const [view, setView] = useState('about');
-  const [parameters, setParameters] = useState({
-    distance: 50,
-    lengthMin: 0,
-    lengthMax: 20,
-    elevationMin: 0,
-    elevationMax: 5000,
-    elevationGainMin: 0,
-    elevationGainMax: 5000,
-    minRating: 0,
-    centralCascades: true,
-    centralWashington: true,
-    easternWashington: true,
-    issaquahAlps: true,
-    mountRainierArea: true,
-    northCascades: true,
-    olympicPeninsula: true,
-    pugetSoundIslands: true,
-    snoqualmieRegion: true,
-    southCascades: true,
-    southwestWashington: true,
-    coast: false,
-    rivers: false,
-    lakes: false,
-    waterfalls: false,
-    oldGrowth: false,
-    fallFoliage: false,
-    wildflowersMeadows: false,
-    mountainViews: false,
-    summits: false,
-    wildlife: false,
-    ridgesPasses: false,
-    establishedCampsites: false,
-    discoverPass: true,
-    northwestForestPass: true,
-    kidFriendly: true,
-    dogFriendly: true
-  });
 
   return (
    <AppContainer className="app">
@@ -58,8 +21,6 @@ const App = () => {
     <ContentWindow
       view={view}
       contentWindowExpanded={contentWindowExpanded}
-      parameters={parameters}
-      setParameters={setParameters}
     />
 
     <Menu
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RangeParameters from './RangeParameters';
 import HikeRegions from './HikeRegions';
 import HikeFeatures from './HikeFeatures';
@@ -6,16 +6,54 @@ import PassRequirements from './PassRequirements';
 import Miscellaneous from './Miscellaneous';
 
 const Search = (props) => {
+  const [parameters, setParameters] = useState({
+    distance: 50,
+    lengthMin: 0,
+    lengthMax: 20,
+    elevationMin: 0,
+    elevationMax: 5000,
+    elevationGainMin: 0,
+    elevationGainMax: 5000,
+    minRating: 0,
+    centralCascades: true,
+    centralWashington: true,
+    easternWashington: true,
+    issaquahAlps: true,
+    mountRainierArea: true,
+    northCascades: true,
+    olympicPeninsula: true,
+    pugetSoundIslands: true,
+    snoqualmieRegion: true,
+    southCascades: true,
+    southwestWashington: true,
+    coast: false,
+    rivers: false,
+    lakes: false,
+    waterfalls: false,
+    oldGrowth: false,
+    fallFoliage: false,
+    wildflowersMeadows: false,
+    mountainViews: false,
+    summits: false,
+    wildlife: false,
+    ridgesPasses: false,
+    establishedCampsites: false,
+    discoverPass: true,
+    northwestForestPass: true,
+    kidFriendly: true,
+    dogFriendly: true
+  });
+
   return(
     <div className="content-section">
       <h2>Search for Hikes</h2>
       <p>Use the form below to search for hikes.</p>
       <form>
-        <RangeParameters parameters={props.parameters} setParameters={props.setParameters} />
-        <HikeRegions parameters={props.parameters} />
-        <HikeFeatures parameters={props.parameters} />
-        <PassRequirements parameters={props.parameters} />
-        <Miscellaneous parameters={props.parameters} />
+        <RangeParameters parameters={parameters} setParameters={setParameters} />
+        <HikeRegions parameters={parameters} />
+        <HikeFeatures parameters={parameters} />
+        <PassRequirements parameters={parameters} />
+        <Miscellaneous parameters={parameters} />
         <button>Search Hikes</button>
       </form>
     </div>
