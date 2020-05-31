7840bd4bb1d9efe9866594f0bc50bbe53c084090
Marcus Price <marcusprice88@gmail.com>
2020-05-31 12:28:41 -0700

    added documentation in app entrypoint

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
-const express = require('express');
+require('dotenv').config();                   //environment variables
+const express = require('express');           //express app
+const port = process.env.PORT || 5000         //in development the client/CRA runs on port 3000
+
+//instantiate express and set the static directory
 const app = express();
-const port = process.env.PORT || 5000
 app.use(express.static('./client/build'));
 
+//api routes
 require('./routes/hikes')(app);
 
 app.listen(port, () => {
